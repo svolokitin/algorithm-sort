a9bc8a1176e411364436edbec1ff0cdce53d5b2e
Serhii Volokitin <volokitin14sv@gmail.com>
2022-10-01 23:41:42 +0300

    modified bubbleSort

--- mx_bubble_sort.js
+++ mx_bubble_sort.js
@@ -6,9 +6,7 @@ function bubble_sort(arr) {
 	for (let i = 0; i < len - 1; i++) {
 		for (let j = 0; j < len - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				let item = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = item;
+				[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
 			}
 		}
 		console.log(i, arr);
