1a120691738a8bdf4c70e09e61e57dca68d0e990
Serhii Volokitin <volokitin14sv@gmail.com>
2022-10-06 14:09:44 +0300

    factorial fibonachi

---
+++ mx_recursion.js
@@ -0,0 +1,16 @@
+const factorial = (n) => {
+	if (n === 1) {
+		return 1;
+	}
+
+	return n * factorial(n - 1);
+}
+
+const fibonachi = (n) => {
+	if (n === 1 || n === 2) {
+		return 1;
+	}
+
+	return fibonachi(n - 1) + fibonachi(n - 2);
+}
+
