6cca4ea16048f3b931b0483c54cd31c137caa195
Serhii Volokitin <volokitin14sv@gmail.com>
2022-10-06 16:58:56 +0300

    add recursive function

--- mx_binary_search.js
+++ mx_binary_search.js
@@ -1,4 +1,4 @@
-const arr = [0, 1, 2, 3, 4, 5, 6];
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 29];
 const len = arr.length;
 let count = 0;
 
@@ -26,5 +26,18 @@ function binarySearch(arr, item) {
 	return position;
 }
 
-console.log(binarySearch(arr, 2));
-console.log('count = ', count);
+function recursiveBinarySearch(arr, item, start, end) {
+	let midle = Math.floor((start + end) / 2);
+
+	if (arr[midle] === item) {
+		return midle;
+	}
+	if (arr[midle] > item) {
+		return recursiveBinarySearch(arr, item, start, midle - 1);
+	}
+	else {
+		return recursiveBinarySearch(arr, item, midle + 1, end);
+	}
+}
+
+console.log(recursiveBinarySearch(arr, 23, 0, arr.length));
